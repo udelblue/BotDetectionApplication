73fdf594e621afd17915eb99fed8cad9f96c0fa5
Christopher Sommers <cdsommers@gmail.com>
2013-06-23 17:43:35 -0400

    update

---
+++ BotDetectionApplication/BotDetector.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BotDetectionApplication
+{
+
+
+
+
+
+    public class BotDetector
+    {
+
+
+        private List<string> _bots;
+        public List<string> bots
+        {
+            get
+            {
+                if (_bots == null)
+                {
+                    _bots = new List<string>(){"008","ABACHOBot","Accoona-AI-Agent","AddSugarSpiderBot","AnyApexBot","Arachmo","B-l-i-t-z-B-O-T","Baiduspider","BecomeBot",
+        "BeslistBot","BillyBobBot","Bimbot","Bingbot","BlitzBOT","boitho.com-dc","boitho.com-robot","btbot","CatchBot","Cerberian Drtrs",
+        "Charlotte","ConveraCrawler","cosmos","Covario IDS","DataparkSearch","DiamondBot","Discobot","Dotbot","EARTHCOM.info","EmeraldShield.com WebBot",
+        "envolk[ITS]spider","EsperanzaBot","Exabot","FAST Enterprise Crawler","FAST-WebCrawler","FDSE robot","FindLinks","FurlBot","FyberSpider",
+        "g2crawler","Gaisbot","GalaxyBot","genieBot","Gigabot","Girafabot","Googlebot-Image","Googlebot","GurujiBot","HappyFunBot","hl_ftien_spider",
+        "Holmes","htdig","iaskspider","ia_archiver","iCCrawler","ichiro","igdeSpyder","IRLbot","IssueCrawler","Jaxified Bot","Jyxobot","KoepaBot",
+        "L.webis","LapozzBot","Larbin","LDSpider","LexxeBot","Linguee Bot","LinkWalker","lmspider","lwp-trivial","mabontland","magpie-crawler",
+        "Mediapartners-Google","MJ12bot","MLBot","Mnogosearch","mogimogi","MojeekBot","Moreoverbot","Morning Paper","msnbot","MSRBot","MVAClient",
+        "mxbot","NetResearchServer","NetSeer Crawler","NewsGator","NG-Search","nicebot","noxtrumbot","Nusearch Spider","NutchCVS","Nymesis","obot",
+        "oegp","omgilibot","OmniExplorer_Bot","OOZBOT","Orbiter","PageBitesHyperBot","Peew","polybot","Pompos","PostPost","Psbot","PycURL","Qseero",
+        "Radian6","RAMPyBot","RufusBot","SandCrawler","SBIder","ScoutJet","Scrubby","SearchSight","Seekbot","semanticdiscovery","Sensis Web Crawler",
+        "SEOChat::Bot","SeznamBot","Shim-Crawler","ShopWiki","Shoula robot","silk","Sitebot","Snappy","sogou spider","Sosospider","Speedy Spider",
+        "Sqworm","StackRambler","suggybot","SurveyBot","SynooBot","Teoma","TerrawizBot","TheSuBot","Thumbnail.CZ robot","TinEye","truwoGPS","TurnitinBot",
+        "TweetedTimes Bot","TwengaBot","updated","Urlfilebot","Vagabondo","VoilaBot","Vortex","voyager","VYU2","webcollage","Websquash.com","wf84",
+        "WoFindeIch Robot","WomlpeFactory","Xaldon_WebSpider","yacy","Yahoo! Slurp","Yahoo! Slurp China","YahooSeeker","YahooSeeker-Testing","YandexBot",
+        "YandexImages","YandexMetrika","Yasaklibot","Yeti","YodaoBot","yoogliFetchAgent","YoudaoBot","Zao","Zealbot","zspider","ZyBorg", "teoma" ,"bot", "spieder", "crawler", "walker"};
+                }
+
+                return _bots;
+            }
+
+        }
+
+
+
+
+
+        private List<string> _botsSearchEngine;
+        public List<string> botsSearchEngine
+        {
+            get
+            {
+                if (_botsSearchEngine == null)
+                {
+                    _botsSearchEngine = new List<string>() { "googlebot", "yahoo", "ask", "bing", "msn", "teoma" };
+                }
+
+                return _botsSearchEngine;
+            }
+
+        }
+
+
+
+
+        public BotDetector(string useragent)
+        {
+            SearchBots(useragent);
+        }
+
+
+        private void SearchBots(string useragent)
+        {
+            bool bot = false;
+            bool searchengine = false;
+            DateTime time = DateTime.Now;
+
+            foreach (string b in bots)
+            {
+                if (useragent.Contains(b))
+                {
+                    bot = true;
+                    time = DateTime.Now;
+                }
+
+
+            }
+
+            foreach (string b in botsSearchEngine)
+            {
+                if (useragent.Contains(b))
+                {
+                    searchengine = true;
+                    time = DateTime.Now;
+                }
+            }
+
+
+            if (bot | searchengine)
+            {
+               string url = HttpContext.Current.Request.Url.AbsoluteUri;
+
+
+                if (bot)
+                {
+                    string subject = "Malicious Engine bot";
+                    StringBuilder message = new StringBuilder();
+                    message.AppendLine("Site: " + url);
+                    message.AppendLine("Time: " + time.ToLongTimeString());
+                    message.AppendLine("Bot Type: " + "Malicious Engine bot");
+                    message.AppendLine("User Agent: " + useragent);
+                    Notification.SendEmailNetwork(subject, message.ToString());
+                }
+
+                if (searchengine)
+                {
+                    string subject = "Search Engine bot";
+                    StringBuilder message = new StringBuilder();
+                    message.AppendLine("Site: " + url);
+                    message.AppendLine("Time: " + time.ToLongTimeString());
+                    message.AppendLine("Bot Type: " + "Search Engine bot");
+                    message.AppendLine("User Agent: " + useragent);
+                    Notification.SendEmailNetwork(subject, message.ToString());
+                }
+
+            }
+
+
+        }
+
+
+    }
+}
---
+++ BotDetectionApplication/Notification.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Data;
+using System.Configuration;
+using System.Linq;
+using System.Web;
+using System.Web.Security;
+using System.Web.UI;
+using System.Web.UI.HtmlControls;
+using System.Web.UI.WebControls;
+using System.Web.UI.WebControls.WebParts;
+using System.Xml.Linq;
+
+using System.Net.Mail;
+
+
+namespace BotDetectionApplication
+{
+    public class Notification
+    {
+
+
+        public static void SendEmailGoogle(string subject, string message)
+        {
+            string host = ConfigurationManager.AppSettings["EmailServerHost"];
+           // int port = Convert.ToInt32(ConfigurationManager.AppSettings["EmailServerPort"]);
+            string domain = ConfigurationManager.AppSettings["EmailServerDomain"];
+            string username = ConfigurationManager.AppSettings["EmailServerUsername"];
+            string password = ConfigurationManager.AppSettings["EmailServerPassword"];
+            string fromEmailAddress = ConfigurationManager.AppSettings["FromEmailAddress"];
+            string toEmailAddress = ConfigurationManager.AppSettings["ToEmailAddress"];
+
+            MailMessage mail = new MailMessage(fromEmailAddress, toEmailAddress);
+            SmtpClient client = new SmtpClient();
+            client.Port = 25;
+            client.DeliveryMethod = SmtpDeliveryMethod.Network;
+            client.UseDefaultCredentials = false;
+            client.Host = "smtp.google.com";
+            mail.Subject = subject;
+            mail.Body = message;
+            client.Send(mail);
+        }
+
+
+
+
+        public static void SendEmailNetwork(string subject, string message)
+        {
+            string host = ConfigurationManager.AppSettings["EmailServerHost"];
+            int port = Convert.ToInt32(ConfigurationManager.AppSettings["EmailServerPort"]);
+            string domain = ConfigurationManager.AppSettings["EmailServerDomain"];
+            string username = ConfigurationManager.AppSettings["EmailServerUsername"];
+            string password = ConfigurationManager.AppSettings["EmailServerPassword"];
+            string fromEmailAddress = ConfigurationManager.AppSettings["FromEmailAddress"];
+
+            string toEmailAddress = ConfigurationManager.AppSettings["ToEmailAddress"];
+
+            MailMessage mailMessage = new MailMessage(fromEmailAddress, toEmailAddress);
+            mailMessage.Subject = subject;
+           
+
+            mailMessage.Body = message;
+            mailMessage.IsBodyHtml = false;
+
+            System.Net.NetworkCredential networkCredential = new System.Net.NetworkCredential(username, password, domain);
+
+            SmtpClient smtpClient = new SmtpClient();
+            smtpClient.Host = host;
+            smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+            smtpClient.Port = port;
+            smtpClient.UseDefaultCredentials = false;
+            smtpClient.Credentials = networkCredential;
+
+            smtpClient.Send(mailMessage);
+
+        }
+
+    }
+}
